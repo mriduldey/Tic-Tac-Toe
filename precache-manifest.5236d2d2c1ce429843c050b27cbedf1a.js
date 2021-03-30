d419c3ec43f346104d50a429aebad1296f5a5184
unknown <mriduldey94@gmail.com>
2021-03-30 13:03:50 +0530

    Updates

---
+++ precache-manifest.5236d2d2c1ce429843c050b27cbedf1a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2e990f1e78776cf9fd41e03bd22595ea",
+    "url": "/Tic-Tac-Toe/index.html"
+  },
+  {
+    "revision": "a2535be3767ab63a3f4b",
+    "url": "/Tic-Tac-Toe/static/css/main.9264e488.chunk.css"
+  },
+  {
+    "revision": "81ca244c645f2453f505",
+    "url": "/Tic-Tac-Toe/static/js/2.084e9ad2.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Tic-Tac-Toe/static/js/2.084e9ad2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a2535be3767ab63a3f4b",
+    "url": "/Tic-Tac-Toe/static/js/main.2c2f9a73.chunk.js"
+  },
+  {
+    "revision": "9b5dbe07129a41c110b2",
+    "url": "/Tic-Tac-Toe/static/js/runtime-main.cfd16560.js"
+  },
+  {
+    "revision": "ee7cd8ed2dcec943251eb2763684fc6f",
+    "url": "/Tic-Tac-Toe/static/media/logo.ee7cd8ed.svg"
+  }
+]);
